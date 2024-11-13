b887281630a0445a91ae0948d60ea25704b7e924
Ziming Tan <albus_tan@outlook.com>
2024-11-13 11:47:19 -0800

    feat: CToBrilVisitor framework

--- bril-c/README.md
+++ bril-c/README.md
@@ -11,5 +11,14 @@ See [Python runtime targets](https://github.com/antlr/antlr4/blob/master/doc/pyt
 Try parsing an example:
 
 ```
- python ./c/driver.py c/examples/add.c
+python ./c/driver.py c/examples/simple.c
+
+# Use debug mode to print syntax tree
+python ./c/driver.py -D c/examples/simple.c
+
+# Check coverted bril program
+python ./c/driver.py c/examples/simple.c | bril2txt
+
+# Run coverted bril program
+python ./c/driver.py c/examples/simple.c | brili -p
 ```
--- bril-c/c/driver.py
+++ bril-c/c/driver.py
@@ -1,17 +1,40 @@
 import sys
+import json
 from antlr4 import *
 from CLexer import CLexer
 from CParser import CParser
 from antlr4.tree.Trees import Trees
+from CToBrilVisitor import CToBrilVisitor
 
 def main(argv):
+    debug_mode = "-D" in argv
+    if debug_mode:
+        argv.remove("-D")
+
     input_stream = FileStream(argv[1])
     lexer = CLexer(input_stream)
     stream = CommonTokenStream(lexer)
     parser = CParser(stream)
     tree = parser.compilationUnit()
-    print(Trees.toStringTree(tree, None, parser))
+
+    if debug_mode:
+        print("Syntax Tree:")
+        print(Trees.toStringTree(tree, None, parser))
+
+    if parser.getNumberOfSyntaxErrors() > 0:
+        print("syntax errors")
+    else:
+        vinterp = CToBrilVisitor()
+        vinterp.visit(tree)
+
+        if debug_mode:
+            print("\nGenerated Bril Program:")
+        
+        json.dump(vinterp.bril_program, sys.stdout, indent=2)
 
 if __name__ == '__main__':
-    main(sys.argv)
+    if len(sys.argv) < 2:
+        print("Usage: python3 driver.py [-D] <c-source-file>")
+    else:
+        main(sys.argv)
 
